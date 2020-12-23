bc28df0e709c067d4c01189f8d68c3c4c00c7ad1
huy2107 <huypl2107@gmail.com>
2020-12-23 09:13:38 +0700

    'toantu'

---
+++ thuchanhtoantu.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt("Enter the witdh");
+inputheight = prompt("Enter the height");
+let witdh = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = witdh * height;
+document.write("The area is:"+ area);
